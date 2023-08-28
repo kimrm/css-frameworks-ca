94c1c67b0f0f8d2bb17fda6314d305e6ae193776
Kim Rune Møller <kimrune@hotmail.no>
2023-08-28 08:27:18 +0200

    add dist folder

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+import "./bootstrap.bundle.min.js";
+
+//
+// Place any custom JS here
+//
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+  new bootstrap.Popover(popover);
+});
