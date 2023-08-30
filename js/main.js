6aa613beaf455e989d9c80051394221709f3808f
Kim Rune Møller <kimrune@hotmail.no>
2023-08-30 10:07:45 +0200

    js cleanup

--- js/main.js
+++ js/main.js
@@ -1,38 +1,18 @@
-// Import the Bootstrap bundle
-//
-// This includes Popper and all of Bootstrap's JS plugins.
-
-// import "./bootstrap.bundle.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-//
-// Place any custom JS here
-//
+const forms = document.querySelectorAll(".needs-validation");
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
-  new bootstrap.Popover(popover);
+Array.from(forms).forEach((form) => {
+  form.addEventListener(
+    "submit",
+    (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add("was-validated");
+    },
+    false
+  );
 });
-
-(() => {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
